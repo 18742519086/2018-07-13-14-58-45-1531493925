e1ca9353dd9b5e225056c94eb8d51a0789eb5661
daibowen <972771202@qq.com>
2018-08-03 12:26:09 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,38 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+	\n o) 最大值 = ${sequence.max()}
+	\n o) 元素数量 = ${sequence.count()
+	\n o) 平均值 = ${sequence.average()}
+	`);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+	this.input = input;
   }
 
   minimum() {
     // Write your code here
+	return Math.min.apply(null, input);
   }
 
   // Write your code here
+  max() {
+    // Write your code here
+	return Math.max.apply(null, input);
+  }
+  
+  count() {
+    // Write your code here
+	return input.length;
+  }
+  
+  average() {
+    // Write your code here
+	var sum = eval(input.join("+"));
+    return ~~(sum/input.length*100)/100;
+  }
 }
+
